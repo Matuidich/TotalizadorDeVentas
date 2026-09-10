08c82e9b84c403811be35d2337ab15f9b19c5e24
matuidich <mateito.rojas793@gmail.com>
2026-09-10 00:37:34 -0400

    Permite ingresar precio decimal con TDD

--- cypress/e2e/sumador.cy.js
+++ cypress/e2e/sumador.cy.js
@@ -4,4 +4,9 @@ describe("Totalizador de venta", () => {
     cy.get('label[for="cantidad"]').should("contain", "Cantidad");
     cy.get("#cantidad").type("20").should("have.value", "20");
   });
+  it("permite ingresar el precio con decimales", () => {
+    cy.visit("/");
+    cy.get('label[for="precio"]').should("contain", "Precio");
+    cy.get("#precio").type("3.50").should("have.value", "3.50");
+  });
 });
