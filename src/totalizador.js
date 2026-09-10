18468387f4dcd9afb2872356d759ee0c1ed05c43
matuidich <mateito.rojas793@gmail.com>
2026-09-09 23:41:16 -0400

    Implementar descuento del cero por ciento para compras menores a 1000

--- src/totalizador.js
+++ src/totalizador.js
@@ -2,4 +2,8 @@ function calcularPrecioNeto(cantidad, precio) {
   return cantidad * precio;
 }
 
+export function obtenerPorcentajeDescuento() {
+  return 0;
+}
+
 export default calcularPrecioNeto;
--- src/totalizador.test.js
+++ src/totalizador.test.js
@@ -1,7 +1,11 @@
-import calcularPrecioNeto from "./totalizador.js";
+import calcularPrecioNeto, { obtenerPorcentajeDescuento } from "./totalizador.js";
 
 describe("Totalizador de venta", () => {
   it("deberia calcular el precio neto de la compra", () => {
     expect(calcularPrecioNeto(20, 3)).toEqual(60);
+  });
+
+  it("deberia aplicar cero por ciento de descuento a compras menores a 1000", () => {
+    expect(obtenerPorcentajeDescuento(999.99)).toEqual(0);
   });
 });
