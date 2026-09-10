38024b45aff8d71c3b079b343401ecb06c6c657b
matuidich <mateito.rojas793@gmail.com>
2026-09-10 00:39:25 -0400

    Permite seleccionar estado de compra con TDD

--- cypress/e2e/sumador.cy.js
+++ cypress/e2e/sumador.cy.js
@@ -9,4 +9,9 @@ describe("Totalizador de venta", () => {
     cy.get('label[for="precio"]').should("contain", "Precio");
     cy.get("#precio").type("3.50").should("have.value", "3.50");
   });
+  it("permite seleccionar el estado de la compra", () => {
+    cy.visit("/");
+    cy.get('label[for="estado"]').should("contain", "Estado");
+    cy.get("#estado").select("TX").should("have.value", "TX");
+  });
 });
